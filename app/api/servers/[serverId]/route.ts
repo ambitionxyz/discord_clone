409309942626a0a3b572d9e7cd0b30e78ff4946c
quang luan <hoangquangluan23102001@gmail.com>
2023-10-06 20:33:35 +0700

    delete & leave server modal

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -22,6 +22,12 @@ export async function PATCH(
       });
     }
 
+    if (!params.serverId) {
+      return new NextResponse("Server ID Missing", {
+        status: 400,
+      });
+    }
+
     const server = await db.server.update({
       where: {
         id: params.serverId,
@@ -36,6 +42,47 @@ export async function PATCH(
     return NextResponse.json(server);
   } catch (error) {
     console.log("[SERVER_ID_PATCH]", error);
+    return new NextResponse("Internal Error", {
+      status: 500,
+    });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  {
+    params,
+  }: {
+    params: {
+      serverId: string;
+    };
+  }
+) {
+  try {
+    const profile = await currentProfile();
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", {
+        status: 401,
+      });
+    }
+
+    if (!params.serverId) {
+      return new NextResponse("Server ID Missing", {
+        status: 400,
+      });
+    }
+
+    const server = await db.server.delete({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVER_ID_DELETE]", error);
     return new NextResponse("Internal Error", {
       status: 500,
     });
